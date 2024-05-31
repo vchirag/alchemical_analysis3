7d642c2a14c01b461acd8fe4b5b768f76d87d5f5
chirag.verma02 <chirag.verma@mpinat.mpg.de>
2024-05-31 16:28:29 +0200

    indent fix in unixlike.py

--- alchemical_analysis/unixlike.py
+++ alchemical_analysis/unixlike.py
@@ -49,38 +49,43 @@ def tailPy(f, nlines, LENB=1024):
       sizeb -= LENB
       i += 1
    return ''.join(excerpt[::-1]).splitlines()[-nlines:]
-
+   
 def grepFromSection(f, section, *stringhe, **kwargs):
-   """From section 'section' of file 'f' extract the values of strings 'stringhe'."""
-   n = 0 if not 'n' in kwargs else kwargs['n']
-   f_tell_0 = f.tell()==0 # Will need later to adjust 'i'.
-   for i, line in enumerate(f):
-      if section in line:
-         if not stringhe:
-            i += (1 if f_tell_0 else 2)
-            # Assumption: will extract the first number found.
-            nsteps = [int(el) for el in line.split() if el.isdigit()][0]
-            return (i+n, nsteps)
-         found = {}
-         try:
-            line = next(f) # The first line after the line with 'section'.
-            i += 1
-	    while not line.strip('\n '): # Skip blank lines at the beginning.
-               line = next(f)
-               i += 1
-            if not f_tell_0:
-               i += 1 # To compensate for the uncounted line.
-	    while line.strip('\n '): # Search until a blank line is encountered.
-               line = trPy(line, '[,:=]')
-               for s in stringhe:
-                  if s in line:
-                     found[s] = line.split(s)[1].split()[0]
-               line = next(f)
-               i += 1 # Will account for the initial 0 when leaving the loop.
-         except StopIteration:
-            pass
-         if not len(found)==len(stringhe):
-            raise SystemExit("\nERROR!\nSection '%s' of file '%s' does not contain \
-string(s): %s." % (section, f.name, ', '.join([s for s in stringhe if s not in found])))
-         return i+n, list(found[s] for s in stringhe)
-   raise SystemExit("\nERROR!\nThere is no section '%s' in file '%s'." % (section, f.name))
+    """From section 'section' of file 'f' extract the values of strings 'stringhe'."""
+    n = 0 if 'n' not in kwargs else kwargs['n']
+    f_tell_0 = f.tell() == 0  # Will need later to adjust 'i'.
+    for i, line in enumerate(f):
+        if section in line:
+            if not stringhe:
+                i += (1 if f_tell_0 else 2)
+                # Assumption: will extract the first number found.
+                nsteps = [int(el) for el in line.split() if el.isdigit()][0]
+                return i + n, nsteps
+            found = {}
+            try:
+                line = next(f)  # The first line after the line with 'section'.
+                i += 1
+                while not line.strip('\n '):  # Skip blank lines at the beginning.
+                    line = next(f)
+                    i += 1
+                if not f_tell_0:
+                    i += 1  # To compensate for the uncounted line.
+                while line.strip('\n '):  # Search until a blank line is encountered.
+                    line = trPy(line, '[,:=]')
+                    for s in stringhe:
+                        if s in line:
+                            found[s] = line.split(s)[1].split()[0]
+                    line = next(f)
+                    i += 1  # Will account for the initial 0 when leaving the loop.
+            except StopIteration:
+                pass
+            if len(found) != len(stringhe):
+                raise SystemExit(
+                    "\nERROR!\nSection '%s' of file '%s' does not contain string(s): %s."
+                    % (section, f.name, ', '.join([s for s in stringhe if s not in found]))
+                )
+            return i + n, [found[s] for s in stringhe]
+    raise SystemExit(
+        "\nERROR!\nThere is no section '%s' in file '%s'." % (section, f.name)
+    )
+
